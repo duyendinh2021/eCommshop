c25d44f0ba7b14b2a86509a8b8bc0acca91995a8
duyendinh2021 <duyendinh2021@outlook.com>
2023-02-17 10:39:40 +0700

    fix f5 firefox

--- js/detail.js
+++ js/detail.js
@@ -3,6 +3,10 @@ $('input[type="checkbox"]').on("click", check_add_card);
 let cost = 0;
 let html_price = document.getElementById("price-select-item");
 
+function uncheck() {
+  $(".check-product-to-card").prop("checked", false);
+}
+
 function check_add_card() {
   if ($(this).prop("checked") == true) {
     cost = cost + Number($(this).val());
@@ -12,6 +16,8 @@ function check_add_card() {
     html_price.innerHTML = `$${cost}`;
   }
 }
+
+window.onbeforeunload = uncheck();
 
 new Splide("#carousel-recently", {
   pagination: true,
